c9a0db43abf4ffe6266121eacb2a43f9ae0fddc1
Teruki TADA <me@peruki.dev>
2024-04-03 00:51:29 +0900

    wip: NextTransportNodeType

--- src/transport/builder.rs
+++ src/transport/builder.rs
@@ -10,6 +10,12 @@ use super::{
     property::TransportPropertyProvider,
 };
 
+enum NextTransportNodeType {
+    New(TransportNode),
+    Existing(TransportNode),
+    Intersect(TransportNode, LineSegment<TransportNode>),
+}
+
 pub struct TransportBuilder<'a, TP>
 where
     TP: TransportPropertyProvider,
@@ -66,10 +72,10 @@ where
     fn determine_node_to_apply(
         &self,
         node_from: &TransportNode,
-        candidate: PathCandidate,
+        candidate: &PathCandidate,
     ) -> Option<TransportNode> {
         // If the curve is None, the path will be always extended to straight.
-        let curve = candidate.curve.unwrap_or_default();
+        let curve = candidate.curve.clone().unwrap_or_default();
 
         candidate
             .angle_to
@@ -92,6 +98,36 @@ where
             .map(|(node_to, _)| node_to)
     }
 
+    fn determine_new_node_type(
+        &self,
+        node_from: &TransportNode,
+        node_to: &TransportNode,
+    ) -> NextTransportNodeType {
+        let line_segment = LineSegment::new(*node_from, *node_to);
+        // search the nearest crossing line segment
+        if let Some(crossing_site) = self
+            .network
+            .search_path_crossing(line_segment)
+            .iter()
+            .filter(|(crossing_line_segment, _)| {
+                crossing_line_segment.0.site != node_from.site
+                    && crossing_line_segment.1.site != node_from.site
+            })
+            .min_by(|(_, site), (_, other_site)| {
+                node_from
+                    .site
+                    .distance_2(site)
+                    .total_cmp(&node_from.site.distance_2(other_site))
+            })
+            .map(|(_, site)| site)
+        {
+            let node_crossing = TransportNode::new(*crossing_site, node_from.elevated_height);
+            NextTransportNodeType::Intersect(node_crossing, line_segment)
+        } else {
+            NextTransportNodeType::New(*node_to)
+        }
+    }
+
     pub fn iterate(mut self) -> Self {
         let prior_candidate = if let Some(candidate) = self.path_candidate_container.pop() {
             candidate
@@ -101,97 +137,58 @@ where
         let node_from = prior_candidate.node_from;
 
         // determine node to apply
-        let (paths_to_add, paths_to_remove, node_next, continue_path) = {
+        let node_type = {
             let node_to =
-                if let Some(node_to) = self.determine_node_to_apply(&node_from, prior_candidate) {
+                if let Some(node_to) = self.determine_node_to_apply(&node_from, &prior_candidate) {
                     node_to
                 } else {
                     return self;
                 };
-            let line_segment = LineSegment::new(node_from, node_to);
-            // search the nearest crossing line segment
-            if let Some((crossing_line_segment, crossing_site)) = self
-                .network
-                .search_path_crossing(line_segment)
-                .iter()
-                .filter(|(crossing_line_segment, _)| {
-                    crossing_line_segment.0.site != node_from.site
-                        && crossing_line_segment.1.site != node_from.site
-                })
-                .min_by(|(_, site), (_, other_site)| {
-                    node_from
-                        .site
-                        .distance_2(site)
-                        .total_cmp(&node_from.site.distance_2(other_site))
-                })
-            {
-                // remove the crossing line segment
-                let paths_to_remove = vec![**crossing_line_segment];
 
-                // add new line segments
-                let node_crossing = TransportNode::new(*crossing_site, node_from.elevated_height);
-
-                let paths_to_add = vec![
-                    LineSegment::new(node_crossing, crossing_line_segment.0),
-                    LineSegment::new(node_crossing, crossing_line_segment.1),
-                    LineSegment::new(node_from, node_crossing),
-                ];
-                (paths_to_add, paths_to_remove, node_crossing, false)
-            } else {
-                // add new line segments
-                (vec![line_segment], vec![], node_to, true)
-            }
+            self.determine_new_node_type(&node_from, &node_to)
         };
 
-        // remove paths
-        paths_to_remove
-            .iter()
-            .for_each(|line_segment: &LineSegment<TransportNode>| {
+        match node_type {
+            NextTransportNodeType::New(node_to) => {
+                self.network.add_path(node_from, node_to);
+            }
+            NextTransportNodeType::Existing(_) => {}
+            NextTransportNodeType::Intersect(node_next, line_segment) => {
                 self.network.remove_path(line_segment.0, line_segment.1);
-            });
+                self.network.add_path(node_from, node_next);
+                self.network.add_path(node_next, line_segment.0);
+                self.network.add_path(node_next, line_segment.1);
+            }
+        }
 
-        // add paths
-        paths_to_add.iter().for_each(|line_segment| {
-            self.network.add_path(line_segment.0, line_segment.1);
-        });
-
-        if continue_path {
-            let straight_angle = node_from.site.get_angle(&node_next.site);
+        if let NextTransportNodeType::New(node_next) = node_type {
             if let Some(property) = self.property_provider.get_property(&node_next.into()) {
                 // add new path candidates
+                let straight_angle = node_from.site.get_angle(&node_next.site);
                 self.path_candidate_container.push(PathCandidate::new(
                     node_next,
                     straight_angle,
                     property.path_length,
                     property.path_priority,
-                    property.curve,
+                    property.curve.clone(),
                 ));
-            }
-            /*
-            let clockwise_angle = straight_angle.right_clockwise();
-            if let Some(property) = self.property_provider.get_property(&node_next.into()) {
-                // add new path candidates
+                /*
                 self.path_candidate_container.push(PathCandidate::new(
                     node_next,
-                    clockwise_angle,
+                    straight_angle.right_clockwise(),
+                    property.path_length,
+                    property.path_priority,
+                    property.curve.clone(),
+                ));
+                self.path_candidate_container.push(PathCandidate::new(
+                    node_next,
+                    straight_angle.right_counterclockwise(),
                     property.path_length,
                     property.path_priority,
                     property.curve,
                 ));
+                */
             }
-
-            let anticlockwise_angle = straight_angle.right_counterclockwise();
-            if let Some(property) = self.property_provider.get_property(&node_next.into()) {
-                // add new path candidates
-                self.path_candidate_container.push(PathCandidate::new(
-                    node_next,
-                    anticlockwise_angle,
-                    property.path_length,
-                    property.path_priority,
-                    property.curve,
-                ));
-            }
-            */
         }
 
         self
