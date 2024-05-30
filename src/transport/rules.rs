3406087c07939d31717854ae28eece58e9c03ae9
Teruki TADA <me@peruki.dev>
2024-05-31 06:41:57 +0900

    wip: rules for bridge

--- src/transport/rules.rs
+++ src/transport/rules.rs
@@ -14,11 +14,17 @@ pub struct TransportRules {
     /// Extra length of the path to search intersections.
     pub path_extra_length_for_intersection: f64,
 
+    /// Maximum elevation difference of the path per 1.0 length.
+    pub max_elevation_diff: f64,
+
     /// Probability of branching. If 1.0, the path will always create branch.
     pub branch_rules: BranchRules,
 
     /// Rules to determine the direction of the path.
     pub path_direction_rules: PathDirectionRules,
+
+    /// Rules to create bridges.
+    pub bridge_rules: BridgeRules,
 }
 
 /// Rules to create branches.
@@ -38,6 +44,32 @@ impl Default for BranchRules {
         Self {
             branch_density: 0.0,
             staging_probability: 0.0,
+        }
+    }
+}
+
+/// Rules to create bridges.
+///
+/// Bridge is the path that connect two distant sites where the normal path cannot be constructed.
+/// For this package, the meaning of bridges includes not only the bridges over rivers or valleys but also tunnels under mountains.
+///
+/// With `Default` values, the path will never create a bridge.
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BridgeRules {
+    /// Maximum length of bridges.
+    pub max_bridge_length: f64,
+
+    /// Number of comparison steps to create a bridge.
+    /// When creating a bridge, the generator checks the nearest land site with no
+    pub comparison_step: usize,
+}
+
+impl Default for BridgeRules {
+    fn default() -> Self {
+        Self {
+            max_bridge_length: 0.0,
+            comparison_step: 0,
         }
     }
 }
